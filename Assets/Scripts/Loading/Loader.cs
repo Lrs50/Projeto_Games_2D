e9e21db53f035670476bab6f85fa6ee275d9b2fc
Lrs50 <lrs5@cin.ufpe.br>
2022-03-31 20:20:44 -0300

    feat: Loader class

---
+++ Assets/Scripts/Loading/Loader.cs
@@ -0,0 +1,53 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System;
+using System.Collections;
+public static class Loader
+{
+
+    private class LoadingMonoBehavior : MonoBehaviour {};
+    public enum Scene{
+        MainMenu,
+        MainHub,
+        Phase1Scene0,
+        Loading,
+    }
+
+    private static Action onLoaderCallBack; 
+    private static AsyncOperation asyncOperation;
+
+    public static void Load(Scene scene){
+    
+        onLoaderCallBack = () =>{
+           GameObject loadingGameObject = new GameObject("Loading Game Object");
+           loadingGameObject.AddComponent<LoadingMonoBehavior>().StartCoroutine(LoadAsync(scene));
+        };
+
+        SceneManager.LoadScene(Scene.Loading.ToString());
+
+    }
+
+    private static IEnumerator LoadAsync(Scene scene){
+        yield return null;
+        asyncOperation =  SceneManager.LoadSceneAsync(scene.ToString());
+        while(!asyncOperation.isDone){
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    public static float GetLoadingProgress(){
+        if(asyncOperation!=null){
+            return asyncOperation.progress;
+        }else{
+            return 1f;
+        }
+    }
+
+    public static void LoaderCallBack(){
+        if(onLoaderCallBack!=null){
+            onLoaderCallBack();
+            onLoaderCallBack = null;
+        }
+    }
+
+}
