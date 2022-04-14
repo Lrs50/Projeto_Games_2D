00972ccb750159529f9cf96ec7ba162b93ac3ccb
Lrs50 <lrs5@cin.ufpe.br>
2022-04-14 20:50:34 -0300

    feat: player animation

--- Assets/Scripts/Player/PlayerMoveState.cs
+++ Assets/Scripts/Player/PlayerMoveState.cs
@@ -6,7 +6,7 @@ public class PlayerMoveState : BaseStatePlayer {
     float canRun = 1f;
 
     public override void EnterState(PlayerStateManager player) {
-    	//player.rb.MovePosition(player.rb.position + player.walkInput * (player.baseSpeed + (player.sprintInput * player.sprintSpeed)) * Time.fixedDeltaTime);
+    	player.numFrames = 4;
     }
 
     public override void UpdateState(PlayerStateManager player) {
@@ -27,25 +27,24 @@ public class PlayerMoveState : BaseStatePlayer {
 
     public override void FixedUpdateState(PlayerStateManager player){
 
+        if(canRun==1){
+            player.spriteRenderer.sprite = player.runAnimation[player.animationOrientation + player.animationFrame*4];
+        }else{
+            player.spriteRenderer.sprite = player.walkAnimation[player.animationOrientation + player.animationFrame*4];
+        }
     
         Vector2 direction = player.walkInput;
-        
-
         if (direction == Vector2.zero){
             ExitState(player);
         	player.SwitchState(player.idleState);
             return;
         }
-
-        
-
         player.rb.AddForce(direction*player.baseSpeed*(1+canRun));
 
         player.rb.velocity = Vector2.ClampMagnitude(player.rb.velocity,player.maxSpeed*(1+canRun*player.sprintSpeed));
         //Debug.Log(player.rb.velocity);
 
-        Quaternion toRotation = Quaternion.LookRotation(Vector3.forward,direction);
-        player.transform.rotation = Quaternion.RotateTowards(player.transform.rotation,toRotation,player.rotationSpeed*player.sprintSpeed);
+        
 		
     }
 
