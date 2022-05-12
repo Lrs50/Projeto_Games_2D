6ceec737f1da721228d494beb9a844f319373635
João Victor Oliveira Santos <jvos@cin.ufpe.br>
2022-05-12 02:38:48 -0300

    feats: credits scene, custom cursors

--- Assets/Scripts/Player/PlayerStateManager.cs
+++ Assets/Scripts/Player/PlayerStateManager.cs
@@ -186,7 +186,11 @@ public class PlayerStateManager : MonoBehaviour
         for(int i=0;i<5;i++){
             manaImages[i] = mana.GetChild(i+1).gameObject.GetComponent<Image>();
         }
-        
+
+        guaranaImg=playerUI.transform.GetChild(2).GetChild(0).GetChild(0).gameObject.GetComponent<Image>();
+        guaranaQtyText=playerUI.transform.GetChild(2).GetChild(0).GetChild(1).gameObject.GetComponent<Text>();
+        jabuticabaImg=playerUI.transform.GetChild(2).GetChild(1).GetChild(0).gameObject.GetComponent<Image>();
+        jabuticabaQtyText=playerUI.transform.GetChild(2).GetChild(1).GetChild(1).gameObject.GetComponent<Text>();
     }
     void Start() {
         currentState = idleState;
